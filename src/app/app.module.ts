262b71e1b11ee2b0fc4dc46a3fee8e7c268556c6
Josvany_G <josvanyg@gmail.com>
2022-07-06 07:10:26 -0600

    navegacion desde code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ToastrService } from './common/toastr.service';
-
 import { EventsAppComponent } from './event-app.component';
+import { CreateEventComponent } from './events/create-event.component';
 import { EventDetailsComponent } from './events/event-details';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
@@ -16,11 +16,12 @@ import { appRoutes } from './routes';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    CreateEventComponent
 
   ],
-  imports: [   
-    BrowserModule,
+  imports: [
+  BrowserModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,14 +1,15 @@
 import { EventsListComponent } from './events/events-list.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
-// import { CreateEventComponent } from './events/create-event.component'
+import { CreateEventComponent } from './events/create-event.component'
 // import { Error404Component } from './errors/404.component'
 // import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 // import { EventListResolver } from './events/events-list-resolver.service'
 import { Routes } from '@angular/router';
 
 export const appRoutes : Routes = [
+  { path: 'events/new', component:CreateEventComponent},
   { path: 'events', component: EventsListComponent },
-  { path: 'events/:id', component: EventDetailsComponent },
+  { path: 'events/:id', component: EventDetailsComponent },  
   { path: '', redirectTo: '/events', pathMatch: 'full'},
 
 ]
