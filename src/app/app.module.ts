51b1ee2e0aba1f13e945252aa05a249cb2a01af4
Josvany_G <josvanyg@gmail.com>
2022-07-06 06:40:12 -0600

    usando rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { ToastrService } from './common/toastr.service';
 
 import { EventsAppComponent } from './event-app.component';
+import { EventDetailsComponent } from './events/event-details';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/nav-bar.component';
-
+import { appRoutes } from './routes';
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventDetailsComponent
 
   ],
-  imports: [ 
-    BrowserModule
+  imports: [   
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     EventService,
-    ToastrService
+    ToastrService,
+    EventService
   ],
   bootstrap: [EventsAppComponent]
 })
